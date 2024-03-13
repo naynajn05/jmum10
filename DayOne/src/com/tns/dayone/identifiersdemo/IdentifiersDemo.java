340c20e1b6a4124b3c655e81bee7c7372495a67f
Hp <Hp@DESKTOP-MEHK842>
2024-03-13 10:05:52 +0530

    DAY 1

---
+++ DayOne/src/com/tns/dayone/identifiersdemo/IdentifiersDemo.java
@@ -0,0 +1,22 @@
+package com.tns.dayone.identifiersdemo;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		//Invalid 
+		//int var 1=12;
+		
+		//int for=20; //keywords can not be identifiers
+		//int @var=23;
+		
+		//Valid
+		int _var=23;
+		int $var=34;
+		int intVar=34;
+		
+		
+		
+
+	}
+
+}
